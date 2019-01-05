a678f5fce99655e0ecb056cbdf220b52115db491
Islam Ahmad <islam.ahmad@jci.com>
2019-01-06 00:26:40 +0530

    test

---
+++ DataObjects/Registration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataObjects
+{
+    public class Registration
+    {
+		private string fistName;
+		private string lastName;
+		private string email;
+		private string passsword;
+
+		public Registration(string fistName, string lastName, string email, string passsword)
+		{
+			this.fistName = fistName;
+			this.lastName = lastName;
+			this.email = email;
+			this.passsword = passsword;
+		}
+	}
+}
